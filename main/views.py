b6d7d18dc3fddfc9b7341989a1c49e5d18e954c3
islambek <56040819+islambek12@users.noreply.github.com>
2021-01-20 12:52:56 +0600

    Update views.py

--- main/views.py
+++ main/views.py
@@ -1,4 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .models import ToDo
+from .models import ToDo, BooksShop
+
+# Create your views here.
+def homepage(request):
+    return render(request, "index.html")
 
 def test(request):
-    return render(request, "islam.html")
+    todo_list = ToDo.objects.all()
+    return render(request, "test.html", {"todo_list": todo_list})
+
+def second(request):
+    return HttpResponse("test 2 page")
+
+def third(request):
+    return HttpResponse("This is page test3")
+
+
+def booksShop(request):
+    book_list = BooksShop.objects.all()
+    return render(request, "books.html", {"book_list": book_list})
+
+
+# def __str__(self):
+#     return self.name
+
+# def __str__(self):
+#     return self.title
+
+
+# def get_absolute_url(self):
+#     return reverse('book-detail', args=[str(self.id)])
+
+# def __str__(self):
+#     return '%s (%s)' % (self.id,self.book.title)
+
+# def get_absolute_url(self):
+#     return reverse('author-detail', args=[str(self.id)])
+
+
+# def __str__(self):
+#     return '%s, %s' % (self.last_name, self.first_name)
+
+# def __str__(self):
+#     return self.headline 
