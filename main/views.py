51244f6aa32eab720e0d05ce68faae95710f7236
islambek <56040819+islambek12@users.noreply.github.com>
2021-01-28 22:34:12 +0600

    Update views.py

--- main/views.py
+++ main/views.py
@@ -1,46 +1,86 @@
-from django.shortcuts import render, HttpResponse
-from .models import ToDo
-from .models import ToDo, BooksShop
+from django.shortcuts import render, HttpResponse, redirect
+from .models import ToDo, BookStore
 
-# Create your views here.
+
 def homepage(request):
     return render(request, "index.html")
 
+
 def test(request):
     todo_list = ToDo.objects.all()
-    return render(request, "test.html", {"todo_list": todo_list})
-
-def second(request):
-    return HttpResponse("test 2 page")
-
-def third(request):
-    return HttpResponse("This is page test3")
+    return render(request, 'todo.html', {"todo_list": todo_list})
 
 
-def booksShop(request):
-    book_list = BooksShop.objects.all()
-    return render(request, "books.html", {"book_list": book_list})
+def bookStore(request):
+    book_list = BookStore.objects.all()
+    return render(request, 'books.html', {"book_list": book_list})
 
 
-# def __str__(self):
-#     return self.name
+def add_todo(request):
+    form = request.POST
+    todo = ToDo(text=form['todo_text'])
+    todo.save()
 
-# def __str__(self):
-#     return self.title
-
-
-# def get_absolute_url(self):
-#     return reverse('book-detail', args=[str(self.id)])
-
-# def __str__(self):
-#     return '%s (%s)' % (self.id,self.book.title)
-
-# def get_absolute_url(self):
-#     return reverse('author-detail', args=[str(self.id)])
+    return redirect(test)
 
 
-# def __str__(self):
-#     return '%s, %s' % (self.last_name, self.first_name)
+def book_add(request):
+    return render(request, 'books-add.html')
 
-# def __str__(self):
-#     return self.headline 
+
+def add_book(request):
+    form = request.POST
+    bookstor = BookStore(title=form['book-title'], subtitle=form['book-subtitle'],
+                         description=form['book-description'], price=form['book-price'],
+                         genre=form['book-genre'], author=form['book-author'], year=form['book-year'])
+    bookstor.save()
+
+    return redirect(bookStore)
+
+
+def delete_todo(request, id):
+    todo = ToDo.objects.get(id=id)
+    todo.delete()
+
+    return redirect(test)
+
+
+def mark_todo(request, id):
+    todo = ToDo.objects.get(id=id)
+    if todo.is_favorite:
+        todo.is_favorite = False
+    else:
+        todo.is_favorite = True
+    todo.save()
+    return redirect(test)
+
+
+def delete_book(request, id):
+    book = BookStore.objects.get(id=id)
+    book.delete()
+
+    return redirect(bookStore)
+
+
+def favorite_book(request, id):
+    book = BookStore.objects.get(id=id)
+    if book.is_favorite:
+        book.is_favorite = False
+    else:
+        book.is_favorite = True
+    book.save()
+    return redirect(bookStore)
+
+
+def book_info(request, id):
+    book = BookStore.objects.get(id=id)
+    return render(request, 'book-detail.html', {'book': book})
+
+
+def close_todo(request, id):
+    todo = ToDo.objects.get(id=id)
+    todo.is_closed = not todo.is_closed
+    todo.save()
+    return redirect(test)
+
+
